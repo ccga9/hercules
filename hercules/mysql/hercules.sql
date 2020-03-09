61ac68066a350256965cbc7f6dc90e79a97972d6
ccga9 <43570239+ccga9@users.noreply.github.com>
2020-03-09 13:41:18 +0100

    BD actualizada: alimentos,entrenamientos,comentarios,recomendaciones,etc

--- hercules/mysql/hercules.sql
+++ hercules/mysql/hercules.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-03-2020 a las 10:29:37
+-- Tiempo de generación: 09-03-2020 a las 13:39:49
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.2
 
@@ -25,6 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `alimentocomida`
+--
+
+CREATE TABLE `alimentocomida` (
+  `idAlimento` varchar(10) NOT NULL,
+  `idRegistro` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alimentos`
+--
+
+CREATE TABLE `alimentos` (
+  `idAlimento` varchar(10) NOT NULL,
+  `nombre` varchar(15) NOT NULL,
+  `caloriasConsumidas` int(4) UNSIGNED NOT NULL,
+  `carbohidratos` int(4) UNSIGNED NOT NULL,
+  `proteínas` int(4) UNSIGNED NOT NULL,
+  `grasas` int(4) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `comentarios`
 --
 
@@ -39,62 +65,68 @@ CREATE TABLE `comentarios` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `comida`
+-- Estructura de tabla para la tabla `entrenamientoejercicio`
 --
 
-CREATE TABLE `comida` (
-  `Nombre` varchar(15) NOT NULL,
-  `Calorias consumidas` int(4) UNSIGNED NOT NULL,
-  `Carbohidratos` int(4) UNSIGNED NOT NULL,
-  `Proteínas` int(4) UNSIGNED NOT NULL,
-  `Grasas` int(4) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ejercicio`
---
-
-CREATE TABLE `ejercicio` (
-  `Nombre` varchar(25) NOT NULL,
-  `Calorias gastadas` int(4) UNSIGNED NOT NULL,
-  `Tipo` varchar(10) NOT NULL,
-  `Descripcion` text NOT NULL,
-  `Multimedia` mediumblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `entrenadores`
---
-
-CREATE TABLE `entrenadores` (
-  `nif` varchar(10) NOT NULL,
-  `titulacion` varchar(30) NOT NULL,
-  `especialidad` varchar(50) NOT NULL,
-  `experiencia` varchar(30) NOT NULL
+CREATE TABLE `entrenamientoejercicio` (
+  `idEntrenamiento` varchar(10) NOT NULL,
+  `idEjercicio` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `prueba`
+-- Estructura de tabla para la tabla `entrenamientos`
 --
 
-CREATE TABLE `prueba` (
-  `nombre` varchar(10) NOT NULL,
-  `edad` int(3) NOT NULL
+CREATE TABLE `entrenamientos` (
+  `idEntrenamiento` varchar(10) NOT NULL,
+  `nombre` varchar(25) CHARACTER SET utf8 NOT NULL,
+  `caloriasGastadas` int(4) UNSIGNED NOT NULL,
+  `tipo` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `descripcion` text CHARACTER SET utf8 NOT NULL,
+  `multimedia` mediumblob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `prueba`
+-- Estructura de tabla para la tabla `recomendaciones`
 --
 
-INSERT INTO `prueba` (`nombre`, `edad`) VALUES
-('uno', 10),
-('value', 2);
+CREATE TABLE `recomendaciones` (
+  `entrenador` varchar(10) NOT NULL,
+  `usuario` varchar(10) NOT NULL,
+  `fecha` date NOT NULL,
+  `tipo` varchar(10) NOT NULL,
+  `recomendacion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registrocomida`
+--
+
+CREATE TABLE `registrocomida` (
+  `dia` date NOT NULL,
+  `tipo` varchar(15) NOT NULL,
+  `usuario` varchar(10) NOT NULL,
+  `comida` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registroejercicio`
+--
+
+CREATE TABLE `registroejercicio` (
+  `fecha` date NOT NULL,
+  `tipo` varchar(15) NOT NULL,
+  `usuario` varchar(10) NOT NULL,
+  `ejercicio` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -103,7 +135,6 @@ INSERT INTO `prueba` (`nombre`, `edad`) VALUES
 --
 
 CREATE TABLE `usuarioentrenador` (
-  `id` varchar(10) NOT NULL,
   `usuario` varchar(10) NOT NULL,
   `entrenador` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -127,12 +158,29 @@ CREATE TABLE `usuarios` (
   `peso` decimal(5,0) NOT NULL,
   `altura` decimal(5,0) NOT NULL,
   `preferencias` varchar(50) NOT NULL,
-  `tipoUsuario` tinyint(1) NOT NULL
+  `tipoUsuario` tinyint(1) NOT NULL,
+  `titulacion` varchar(30) NOT NULL,
+  `especialidad` varchar(50) NOT NULL,
+  `experiencia` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `alimentocomida`
+--
+ALTER TABLE `alimentocomida`
+  ADD PRIMARY KEY (`idAlimento`,`idRegistro`),
+  ADD KEY `idalimento` (`idAlimento`,`idRegistro`),
+  ADD KEY `idRegistro` (`idRegistro`);
+
+--
+-- Indices de la tabla `alimentos`
+--
+ALTER TABLE `alimentos`
+  ADD PRIMARY KEY (`idAlimento`);
 
 --
 -- Indices de la tabla `comentarios`
@@ -142,23 +190,49 @@ ALTER TABLE `comentarios`
   ADD KEY `entrenador` (`entrenador`);
 
 --
--- Indices de la tabla `entrenadores`
+-- Indices de la tabla `entrenamientoejercicio`
 --
-ALTER TABLE `entrenadores`
-  ADD PRIMARY KEY (`nif`);
+ALTER TABLE `entrenamientoejercicio`
+  ADD PRIMARY KEY (`idEntrenamiento`,`idEjercicio`),
+  ADD KEY `idEntrenamiento` (`idEntrenamiento`,`idEjercicio`),
+  ADD KEY `idEjercicio` (`idEjercicio`);
 
 --
--- Indices de la tabla `prueba`
+-- Indices de la tabla `entrenamientos`
 --
-ALTER TABLE `prueba`
-  ADD PRIMARY KEY (`nombre`);
+ALTER TABLE `entrenamientos`
+  ADD PRIMARY KEY (`idEntrenamiento`);
+
+--
+-- Indices de la tabla `recomendaciones`
+--
+ALTER TABLE `recomendaciones`
+  ADD PRIMARY KEY (`entrenador`),
+  ADD KEY `usuario` (`usuario`),
+  ADD KEY `tipo` (`tipo`);
+
+--
+-- Indices de la tabla `registrocomida`
+--
+ALTER TABLE `registrocomida`
+  ADD PRIMARY KEY (`tipo`,`dia`),
+  ADD KEY `usuario` (`usuario`),
+  ADD KEY `comida` (`comida`);
+
+--
+-- Indices de la tabla `registroejercicio`
+--
+ALTER TABLE `registroejercicio`
+  ADD PRIMARY KEY (`fecha`,`tipo`),
+  ADD KEY `usuario` (`usuario`),
+  ADD KEY `ejercicio` (`ejercicio`);
 
 --
 -- Indices de la tabla `usuarioentrenador`
 --
 ALTER TABLE `usuarioentrenador`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usuario` (`usuario`),
+  ADD PRIMARY KEY (`usuario`,`entrenador`),
+  ADD KEY `usuario` (`usuario`,`entrenador`),
   ADD KEY `entrenador` (`entrenador`);
 
 --
@@ -172,17 +246,56 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- Filtros para la tabla `alimentocomida`
+--
+ALTER TABLE `alimentocomida`
+  ADD CONSTRAINT `alimentocomida_ibfk_1` FOREIGN KEY (`idAlimento`) REFERENCES `alimentos` (`idAlimento`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `alimentocomida_ibfk_2` FOREIGN KEY (`idRegistro`) REFERENCES `registrocomida` (`comida`) ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
   ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarioentrenador` (`usuario`) ON UPDATE CASCADE;
 
 --
+-- Filtros para la tabla `entrenamientoejercicio`
+--
+ALTER TABLE `entrenamientoejercicio`
+  ADD CONSTRAINT `entrenamientoejercicio_ibfk_1` FOREIGN KEY (`idEjercicio`) REFERENCES `registroejercicio` (`ejercicio`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `entrenamientoejercicio_ibfk_2` FOREIGN KEY (`idEntrenamiento`) REFERENCES `entrenamientos` (`idEntrenamiento`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `recomendaciones`
+--
+ALTER TABLE `recomendaciones`
+  ADD CONSTRAINT `recomendaciones_ibfk_1` FOREIGN KEY (`entrenador`) REFERENCES `usuarioentrenador` (`entrenador`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `recomendaciones_ibfk_2` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`nif`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `registrocomida`
+--
+ALTER TABLE `registrocomida`
+  ADD CONSTRAINT `registrocomida_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`nif`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `registroejercicio`
+--
+ALTER TABLE `registroejercicio`
+  ADD CONSTRAINT `registroejercicio_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`nif`) ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `usuarioentrenador`
 --
 ALTER TABLE `usuarioentrenador`
-  ADD CONSTRAINT `usuarioentrenador_ibfk_1` FOREIGN KEY (`entrenador`) REFERENCES `entrenadores` (`nif`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuarioentrenador_ibfk_1` FOREIGN KEY (`entrenador`) REFERENCES `usuarios` (`nif`) ON UPDATE CASCADE,
   ADD CONSTRAINT `usuarioentrenador_ibfk_2` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`nif`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`nif`) REFERENCES `comentarios` (`entrenador`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
