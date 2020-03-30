3f50223d603638b3daaa13161f606c8fb359e619
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2020-03-30 17:47:10 +0200

    Lo mismo de antes

--- hercules/includes/DAOs/alimentoDAO.php
+++ hercules/includes/DAOs/alimentoDAO.php
@@ -9,10 +9,22 @@ class alimentoDAO extends DAO
         parent::__construct();
     }
 
+    public function listarAlimentos()
+    {
+    	$query = "SELECT nombre from alimento";
+
+    	/*$nombres = array();
+    	while ($query['nombre'])
+    	{
+    		array_push($nombres, $query['nombre']);
+    	}*/
+    	return $this->consultar($query);
+    }
+
     public function consulta($idAlimento)
     {
     	$filas = SelectArray("SELECT * from alimento where idAlimento = '\"$idAlimento\"'");
-    	$fila = $filas[0];
+    	/*$fila = $filas[0];
 
     	$a = new alimento();
     	$a->get_idAlimento() = $fila['idAlimento'];
@@ -22,10 +34,31 @@ class alimentoDAO extends DAO
     	$a->get_proteínas() = $fila['proteínas'];
     	$a->get_grasas() = $fila['grasas'];
 
-    	return $a;
+    	return $a;*/
+
+    	return $this->consultar($query);
     }
 
-    //...
-    
+    public function inserta(alimento $a)
+    {
+    	$query = "INSERT into alimento (nombre, caloriasConsumidas, carbohidratos, proteínas, grasas) values (\"$a->get_nombre()\"','\"$a->get_caloríasConsumidas()\"','\"$a->get_carbohidratos()\"','\"$a->get_proteínas()\"','\"$a->get_grasas()\"') where idAlimento = '\"$a->get_idAlimento()\"'";
+
+    	return $this->insertar($query);
+    }
+
+    public function actualiza(alimento $a)
+    {
+    	$query = "UPDATE alimento (nombre, caloriasConsumidas, carbohidratos, proteínas, grasas) values (\"$a->get_nombre()\"','\"$a->get_caloríasConsumidas()\"','\"$a->get_carbohidratos()\"','\"$a->get_proteínas()\"','\"$a->get_grasas()\"') where idAlimento = '\"$a->get_idAlimento()\"'";
+
+    	return $this->modificar($query);
+    }
+
+    public function elimina(alimento $a)
+    {
+    	$query = "DELETE alimento where idAlimento = '\"$a->get_idAlimento()\"'";
+
+    	return $this->eliminar($query);
+    }
+
 }
 ?>
