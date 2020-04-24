e1f35638da102d7a1773740adfa9d4ff81be2b4a
ccga9 <43570239+ccga9@users.noreply.github.com>
2020-04-24 21:05:24 +0200

    Menu y banner principal

--- hercules/index.php
+++ hercules/index.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="includes/estilo.css" />
+	<link rel="stylesheet" type="text/css" href="includes/estiloPagPrincipal.css" />
 	<meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />
 	<title>HERCULES</title>
 </head>
@@ -19,16 +19,100 @@
 
 	?>
 
+	<!-- Banner principal -->
+	<div class="main-banner" id="main-banner">
+		<div class="imgban" id="imgban1">
+			<div class="imgban-box">
+				<h2>Bienvenido a Hércules</h2>
+				<p>Comienza a navegar ya en nuestro sitio</p>
+			</div>
+		</div>
+		<div class="imgban" id="imgban2">
+			<div class="imgban-box">
+				<h2>¡Regístrate o inicia sesión para disfrutar de todas las ventajas!</h2>
+				<p>Consulta el histórico de tus ejercicios realizados...</p>
+			</div>
+		</div>
+		<div class="imgban" id="imgban3">
+			<div class="imgban-box">
+				<h2>Conoce a nuestros Entrenadores</h2>
+			</div>
+		</div>
+	</div>
 
 	<div id="contenido">
-		<h1 align="center">Bienvenido a Hercules</h1>
-		
-		
+
+		<!-- Script JavaScript del Banner principal -->
+		<script type="text/javascript">
+			var bannerStatus = 1;
+			var bannerTimer = 4000;
+
+			window.onload = function (){
+				bannerLoop();
+			}
+
+			var startBannerLoop = setInterval(function(){
+				bannerLoop();
+			}, bannerTimer);
+
+			function bannerLoop(){
+				if (bannerStatus === 1){
+					document.getElementById("imgban2").style.opacity = "0";
+					setTimeout(function(){
+						document.getElementById("imgban1").style.right = "0px";
+						document.getElementById("imgban1").style.zIndex = "1000";
+						document.getElementById("imgban2").style.right = "-100%";
+						document.getElementById("imgban2").style.zIndex = "1500";
+						document.getElementById("imgban3").style.right = "100%";
+						document.getElementById("imgban3").style.zIndex = "500";
+					}, 500);
+					setTimeout(function(){
+						document.getElementById("imgban2").style.opacity = "1";
+					}, 1000);
+					bannerStatus = 2;
+				}
+
+				else if (bannerStatus === 2){
+					document.getElementById("imgban3").style.opacity = "0";
+					setTimeout(function(){
+						document.getElementById("imgban2").style.right = "0px";
+						document.getElementById("imgban2").style.zIndex = "1000";
+						document.getElementById("imgban3").style.right = "-100%";
+						document.getElementById("imgban3").style.zIndex = "1500";
+						document.getElementById("imgban1").style.right = "100%";
+						document.getElementById("imgban1").style.zIndex = "500";
+					}, 500);
+					setTimeout(function(){
+						document.getElementById("imgban3").style.opacity = "1";
+					}, 1000);
+					bannerStatus = 3;
+				}
+
+				else if (bannerStatus === 3){
+					document.getElementById("imgban1").style.opacity = "0";
+					setTimeout(function(){
+						document.getElementById("imgban3").style.right = "0px";
+						document.getElementById("imgban3").style.zIndex = "1000";
+						document.getElementById("imgban1").style.right = "-100%";
+						document.getElementById("imgban1").style.zIndex = "1500";
+						document.getElementById("imgban2").style.right = "100%";
+						document.getElementById("imgban2").style.zIndex = "500";
+					}, 500);
+					setTimeout(function(){
+						document.getElementById("imgban1").style.opacity = "1";
+					}, 1000);
+					bannerStatus = 1;
+				}
+
+
+			}
+
+		</script>
+
 		<p >Nuestra plataforma está especialmente diseñada para que los 
 		usuarios puedan ejercitarse sin necesidad de desplazarse ni de
 		quedarse a una hora en concreta  también dando la posibilidad de 
 		poder realizar registro y seguimiento del entrenamiento .</p>
-		<img src='includes/img/foto_inicio.jpg' align="middle" alt ="Chica haciendo plank" height ="300" width="450" >
 
 	</div>
 		
