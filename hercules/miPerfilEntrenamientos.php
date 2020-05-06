b6a43d5d3c52a7b18d309d5ed0c160b7726e34b9
miriamCR <miriamcabana96@gmail.com>
2020-05-06 04:15:08 +0200

    tabla de entrena v2

--- hercules/miPerfilEntrenamientos.php
+++ hercules/miPerfilEntrenamientos.php
@@ -19,58 +19,59 @@
 		require('miPerfilCabecera.php');
 	?>
 
-	<h1>Entrenamientos</h1>
-	<img src="https://i.pinimg.com/originals/d0/a2/83/d0a2839695fbbf7f760b4aeabee30957.gif" alt="quote" class= "gif" />
-		
-		<?php
-		if($_SESSION['usuario']->getTipoUsuario()){
-		    $idUsuarioEntrenador = $ctrl->idUsuarioEntrenador($_SESSION['usuario']->getNif(), $_GET['idCliente']);    
-		}else{
-			$idUsuarioEntrenador = $ctrl->idUsuarioEntrenador($_GET['idEntrenador'], $_SESSION['usuario']->getNif());
-		}
-			$entrenamientos = $ctrl->listarEntrenamientos($idUsuarioEntrenador);
-
-			if(count($entrenamientos) > 0){
-			    echo '<table class="tablaEntrenamientos">';
-				
-		
-				foreach ($entrenamientos as $entrenamiento) {
-					echo '<tr>'.'<th>Nombre</th>'.'<th>Fecha</th>'.'<th>Numero de Repeticiones</th>'.'</tr>';
-
-					echo '<tr>';
-						echo '<td>'.$entrenamiento['nombre'].'</td>';
-					    echo '<td>'.$entrenamiento['fecha'].'</td>';
-					    echo '<td>'.$entrenamiento['repeticiones'].'</td>';
-					    
-					    
-
-					    if(count($entrenamiento['ejercicios']) > 0){
-							 		echo '<tr>'.'<th>Ejercicios</th>'.'</tr>';
-							 foreach ($entrenamiento['ejercicios'] as $value) {
-							 		echo '<tr>';
-
-									echo '<td>'.$value['nombreEjercicio'].'</td>';
-									echo '<td>'.$value['caloriasGastadas'].'</td>';
-									echo '<td>'.$value['descripcion'].'</td>';
-									echo '<td>'.'<img src='.$value['multimedia'].' alt="foto" class= "fotos" />'.'</td>';
-								    
-								    echo '</tr>';
-									
-					    	}
-					    	
-					    	
-					    }else{
-					    	echo "No tienes ejercicios.";
-					    }
-						
-				}
-				echo '</table>';
-			 }
-			 else {
-			     echo "Todavía no tienes entrenamientos.";
-			 }
+	<div id="contenido">
+		<h1>Entrenamientos</h1>
+		<img src="https://i.pinimg.com/originals/d0/a2/83/d0a2839695fbbf7f760b4aeabee30957.gif" alt="quote" class= "gif" />
 			
-		?>
+			<?php
+			if($_SESSION['usuario']->getTipoUsuario()){
+			    $idUsuarioEntrenador = $ctrl->idUsuarioEntrenador($_SESSION['usuario']->getNif(), $_GET['idCliente']);    
+			}else{
+				$idUsuarioEntrenador = $ctrl->idUsuarioEntrenador($_GET['idEntrenador'], $_SESSION['usuario']->getNif());
+			}
+				$entrenamientos = $ctrl->listarEntrenamientos($idUsuarioEntrenador);
+
+				if(count($entrenamientos) > 0){
+				    echo '<table class="tablaEntrenamientos">';
+					
+			
+					foreach ($entrenamientos as $entrenamiento) {
+						echo '<tr>'.'<th>Nombre</th>'.'<th>Fecha</th>'.'<th>Numero de Repeticiones</th>'.'</tr>';
+
+						echo '<tr>';
+							echo '<td>'.$entrenamiento['nombre'].'</td>';
+						    echo '<td>'.$entrenamiento['fecha'].'</td>';
+						    echo '<td>'.$entrenamiento['repeticiones'].'</td>';
+						    
+						    
+
+						    if(count($entrenamiento['ejercicios']) > 0){
+								 		echo '<tr>'.'<th>Ejercicios</th>'.'</tr>';
+								 foreach ($entrenamiento['ejercicios'] as $value) {
+								 		echo '<tr>';
+
+										echo '<td>'.$value['nombreEjercicio'].'</td>';
+										echo '<td>'.$value['caloriasGastadas'].'</td>';
+										echo '<td>'.$value['descripcion'].'</td>';
+										echo '<td>'.'<img src='.$value['multimedia'].' alt="foto" class= "fotos" />'.'</td>';
+									    
+									    echo '</tr>';
+										
+						    	}
+						    	
+						    	
+						    }else{
+						    	echo "No tienes ejercicios.";
+						    }
+							
+					}
+					echo '</table>';
+				 }
+				 else {
+				     echo "<p> Todavía no tienes entrenamientos.</p>";
+				 }
+				
+			?>
 
 
 	</div>
