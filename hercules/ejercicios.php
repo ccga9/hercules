9c6130f1e9648b9399a5279ab29049e5172643db
ccga9 <43570239+ccga9@users.noreply.github.com>
2020-05-20 20:22:40 +0200

    mas busqueda

--- hercules/ejercicios.php
+++ hercules/ejercicios.php
@@ -42,7 +42,7 @@
 				$busqueda=trim($_POST['busqueda']);
 
 						if(empty($busqueda)){
-							echo 'Busqueda sin resultados';
+							echo 'No se han encontrado ejercicios. Intenta otro nombre';
 						}
 						else {
 							$encontrado=$ctrl->buscarEjercicio($_POST['busqueda']);
@@ -72,8 +72,50 @@
 			}
 
 			else {
-				$ejercicios= $ctrl->listarTodosEjercicios();
-						echo '<div class="entrenadores-all">';
+
+		$items_page = 8; //Resultados por pagina
+		$page = isset($_GET['p'])?  $_GET['p'] : 1;
+
+		if ($page < 1) {
+		    $page = 1;
+		}
+		//echo $page;
+
+		//Consulta
+		$ejercicios= $ctrl->listarTodosEjercicios();
+		$max = count($ejercicios);
+		//echo $max .'<br>';
+		//Determinar el numero de paginas disponibles
+		$number_of_pages = ceil($max/$items_page);
+		//echo $number_of_pages;
+
+		//Determinar el limite que se muestra en la pagina
+		//$arr= $ctrl->listarTodosEjercicios("LIMIT 0,5");
+		$arr = $ctrl->listarTodosEjercicios("ORDER BY nombre LIMIT ".($page - 1) * $items_page. ",".$items_page);
+		//echo count($arr);
+		if (count($arr) > 0) {
+		        echo '<div class="entrenadores-all">';
+		        echo '<ul>';
+				foreach ($arr as $key => $valor) {
+				    echo '<li>';
+				    echo '<h4>'.$valor['nombre'].'</h4>';
+					echo '<div class="ejercicio"><img src='.$valor['multimedia'].' alt="foto" class= "fotos"/></div>';
+					echo 'Calorías Gastadas: '.$valor['caloriasGastadas'].'<br>';
+					echo 'Tipo: '.$valor['tipo'].'<br>';
+				    echo '</li>';
+				}
+				echo '</ul>';
+				echo '</div>';
+				
+				/*for($page=1;$page<=$number_of_pages;$page++){
+					echo '<a href="ejercicios.php?page='. $page .  '">' . $page . '</a>';
+				}*/
+		}
+
+
+
+						//NO BORRAR
+						/*echo '<div class="entrenadores-all">';
 							echo '<ul>';
 								foreach ($ejercicios as $valor) {
 									echo '<li>';
@@ -85,7 +127,7 @@
 									echo '</li>';						    
 								}
 							echo '</ul>';
-						echo '</div>';
+						echo '</div>';*/
 			}
 		?>
 
@@ -93,6 +135,38 @@
 
 	<?php
 
+	 	echo '<div class="cont-marcador">';
+	    echo '<div class="marcador-pagina">';
+		if($number_of_pages > 0){
+			if($page > 1){
+				echo "<a href=ejercicios.php>";
+				$aux=$page-1;
+				echo "<a href= ejercicios.php?p=". $aux ."><</a>";
+			}
+
+			$i = $page-3;
+			$j =0;
+			while($j < 7){
+				if ($i >= 1 && $i <= $number_of_pages) {
+					if ($i == $page) {
+						echo '<a class="active" href=ejercicios.php?p='.$i.">".$i."</a>";
+					}
+					else{
+						echo "<a href= ejercicios.php?p=".$i.">".$i."</a>";
+					}
+				}
+				$i++;
+	           	$j++;
+			}
+
+			if ($page < $number_of_pages) {
+		        $aux=$page + 1;
+		        echo "<a href= ejercicios.php?p=". $aux .">></a>";
+		        echo "<a href= ejercicios.php?p=". $number_of_pages .">>></a>";
+		    }
+		}
+   		echo '</div>';
+	    echo '</div>';
 		require('includes/comun/pie.php');
 
 	?>
